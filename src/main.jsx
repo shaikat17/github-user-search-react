fe7a1a3f5ce41eff8eb4107dc0efeeb644a6ea58
shaikat17 <shaikatpal56@gmail.com>
2023-04-26 22:53:46 +0600

    working on react routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
 
+import { Error, Login } from './pages'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    errorElement: <Error></Error>,
+  },
+  {
+    path: '/login',
+    element: <Login></Login>
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/pages/AuthWrapper.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthWrapper = () => {
+  return (
+    <div>
+      <h1>AuthWrapper</h1>
+    </div>
+  );
+};
+
+export default AuthWrapper;

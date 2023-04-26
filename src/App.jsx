39a501d4dc4f1c208bfca8b540712b409cf77642
shaikat17 <shaikatpal56@gmail.com>
2023-04-26 23:28:50 +0600

    made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import { Dashboard } from "./pages";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>

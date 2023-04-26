be16e2ceb17ea515fd95c3343baa127cbab0f5ae
shaikat17 <shaikatpal56@gmail.com>
2023-04-26 23:07:39 +0600

    error page design complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Outlet } from 'react-router-dom'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Outlet } from "react-router-dom";
+import { Dashboard } from "./pages";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <h1>Hello</h1>
       <Outlet></Outlet>
+      <Dashboard></Dashboard>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
